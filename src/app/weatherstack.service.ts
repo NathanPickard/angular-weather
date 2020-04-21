baa95a74f5efdba757be6a3dc5fb7d1d346bc9f0
Nathan Pickard <nathan.pickard87@gmail.com>
2020-04-20 21:08:01 -0700

    getWeather method in weatherstack service

--- src/app/weatherstack.service.ts
+++ src/app/weatherstack.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherstackService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeather(location) {
+    // return this.http.get()
+  }
 }
